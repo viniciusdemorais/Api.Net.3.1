269d6086a3009499b64b91349e6c683f696383f5
Vinicius de Morais <viniciusdemoraisbh@gmail.com>
2020-03-03 11:35:00 -0300

    first commit

---
+++ Api/Controllers/PingController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PingController : Controller
+    {
+        [HttpGet]
+        public IActionResult Ping()
+        {
+            return Ok("Pong");
+        }
+    }
+}
